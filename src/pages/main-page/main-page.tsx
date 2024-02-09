c858d7d49a6132503bb7841cc62da9b740804619
UTYFjs <utyf-90@mail.ru>
2024-02-09 12:52:59 +0300

    feat: add basic loayout App

--- src/pages/main-page/main-page.tsx
+++ src/pages/main-page/main-page.tsx
@@ -1,34 +1,87 @@
 import React, { useState } from 'react';
 
-import reactLogo from '/react.svg';
-import viteLogo from '/vite.svg';
-import tsLogo from '/ts.svg';
+import 'antd/dist/antd.css';
 import './main-page.css';
+import { Layout, Menu, Typography } from 'antd';
+import { CalendarTwoTone, HeartFilled, MenuFoldOutlined, MenuUnfoldOutlined, TrophyFilled, UploadOutlined, UserOutlined, VideoCameraOutlined } from '@ant-design/icons';
+import Profile from 'public/profile.svg'
+
+const { Header, Content, Sider } = Layout;
+const {Title, Text} = Typography;
+
+
 
 export const MainPage: React.FC = () => {
-    const [count, setCount] = useState(0);
+   const [collapsed, setCollapsed] = useState(false);
 
-    return (
-        <>
-            <div>
-                <a href='https://vitejs.dev' target='_blank'>
-                    <img src={viteLogo} className='logo' alt='Vite logo' />
-                </a>
-                <a href='https://react.dev' target='_blank'>
-                    <img src={reactLogo} className='logo react' alt='React logo' />
-                </a>
-                <a href='https://www.typescriptlang.org/' target='_blank'>
-                    <img src={tsLogo} className='logo' alt='TS logo' />
-                </a>
-            </div>
-            <h1>Vite + React + TS</h1>
-            <div className='card'>
-                <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-                <p>
-                    Edit <code>src/pages/main-page.tsx</code> and save to test HMR
-                </p>
-            </div>
-            <p className='read-the-docs'>Click on the Vite and React logos to learn more</p>
-        </>
-    );
+   return (
+       <Layout className='app'>
+           <Sider
+               trigger={null}
+               collapsible
+               collapsed={collapsed}
+               style={{ width: 208 }}
+               collapsedWidth={64}
+               width={208}
+           >
+               <div className='logo trigger' onClick={() => setCollapsed(!collapsed)}>
+                   {collapsed ? 'logo2' : 'logo1'}
+                   <img
+                       src={
+                           collapsed ? '/public/cleverFit_large.svg' : '/public/cleverFit_large.svg'
+                       }
+                   />
+               </div>
+               <Menu
+                   theme='light'
+                   mode='inline'
+                   defaultSelectedKeys={['1']}
+                   items={[
+                       {
+                           key: '1',
+                           icon: (
+                               <CalendarTwoTone twoToneColor='--character-light-primary-inverse' />
+                           ),
+                           label: 'Календарь',
+                       },
+                       {
+                           key: '2',
+                           icon: <HeartFilled />,
+                           label: 'Тренировки',
+                       },
+                       {
+                           key: '3',
+                           icon: <TrophyFilled />,
+                           label: 'Достижения',
+                       },
+                       {
+                           key: '4',
+                           icon: <UploadOutlined />,
+                           label: 'Профиль',
+                       },
+                   ]}
+               />
+           </Sider>
+           <Layout className='site-layout' style={{width: collapsed ? 1376: 1232}}>
+               <Header className='header' style={{ padding: 0 }}>
+                   <Text>Главная</Text>
+                   <Title className='title_header'>
+                       Приветствуем тебя в приложении CleverFit - приложении, которое поможет тебе
+                       добиться своей мечты!
+                   </Title>
+               </Header>
+               <Content
+                   className='main'
+                   style={{
+                       padding: 24,
+                       minHeight: 280,
+                   }}
+               >
+                   <Text>
+
+                   </Text>
+               </Content>
+           </Layout>
+       </Layout>
+   );
 };
